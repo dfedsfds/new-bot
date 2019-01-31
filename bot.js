149bf89ecc9089e8b4f8177a05df608452277fd3
dfedsfds <45593336+dfedsfds@users.noreply.github.com>
2019-01-31 13:02:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -155,7 +155,7 @@ client.on('message', message => {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "by Fras#9999";
+    let copy = "";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
@@ -172,7 +172,7 @@ client.on('message', message => {
     message.guild.members.forEach(m => {
   
   var bc = new
-       Discord.RichEmbed()
+       Discord.Rich()
        .setColor('RANDOM')
        .setTitle('Broadcast')
        .addField('سيرفر', message.guild.name)
