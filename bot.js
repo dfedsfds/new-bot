91b3af2942ee0238dede3388c3a87fa5a1b34472
dfedsfds <45593336+dfedsfds@users.noreply.github.com>
2019-01-31 19:10:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,37 @@
-const Discord = require('discord.js');
-const Baron = new Discord.Client();
-const prefix = '$';
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log(`Hixxx online`);
+
+});
+//Hixx
+client.on('message', message => {
+if (message.content.split(' ')[0] == '-m')
+ message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("CONNECT"))  return;
+member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                            message.delete();
+});
+});
+
+//جميع الحقوق محفوظه لهيكس وخبز
 client.on("message", message => {
     var prefix = "$";
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("**bc <message>**");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                m.send(args);
-            });
-            const AziRo = new Discord.RichEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL)   
-            .setTitle('✅| جاري ارسال رسالتك ') 
-            .addBlankField(true)
-            .addField('♨| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
-            .addField('📝| الرسالة ', args)
-            .setColor('RANDOM')  
-            message.channel.sendEmbed(AziRo);          
-        }
-        } else {
-            return;
-        }
-    });
-
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "m")) {
+                         if (!message.member.hasPermission("CONNECT"))  return;
+                            let embed4 = new Discord.RichEmbed()
+             .setDescription("**:white_check_mark: | جاري ارسال البرودكاست**")
+           .addField("مرسل البرودكاست" , message.author)
+          .addField("نص البرودكاست" ,args.join("  "))
+                            .addField("عدد الاعضاء المرسل لهم :busts_in_silhouette:" ,` **[${message.guild.memberCount}]**`,true)
+                                                            .setColor("#008000")
+                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            
+                          }
+});
 
 
 client.login(process.env.BOT_TOKEN);
